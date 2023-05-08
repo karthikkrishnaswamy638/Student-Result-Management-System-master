60f10767f1e58989a4dea153883ba34e21922c89
karthikkrishnaswamy638@gmail.com <karthikkrishnaswamy638@gmail.com>
2023-05-08 09:56:12 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,70 @@
+<?php include('topbar.php');?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/aboutus.css">
+</head>
+<body>
+
+<div class="about-section">
+  <h1>Team Techie</h1>
+  <p>We are a team of 4 members colaborating in a project and made all possible efforts over nights to make it hope you like it.</p>
+  <p> hope this may give us a good Results</p>
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+     <div class="column">
+          <div class="card">
+               <img src="img/logo.png" alt="Karthik" style="width:100%">
+               <div class="container">
+                    <h2>Karthik</h2>
+                    <p class="title">TEAM:- Techie</p>
+                    <p>Ph No.   :   +91 - 6385611298</p>
+                    <p>karthikkrishnaswamy638@gmail.com</p>
+                    <p><a href="https://drive.google.com/file/d/1_ytqi2AGdV3CRPm_ehEf8h1OUOiTldkh/view?usp=drivesdk"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+
+     <div class="column">
+          <div class="card">
+               <img src="img/rahul.jpg" alt="Rahul Rakesh" style="width:100%">
+               <div class="container">
+                    <h2>Rahul Rakesh</h2>
+                    <p class="title">TEAM:- Techie</p>
+                    <p>Ph No.   :   +91 - 7735953025</p>
+                    <p>2711@example.com</p>
+                    <p><a href="#"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+     <div class="column">
+          <div class="card">
+               <img src="img/hp.jpg" alt="Hariprasanth" style="width:100%">
+               <div class="container">
+                    <h2>Hariprasanth</h2>
+                    <p class="title">TEAM:- Techie</p>
+                    <p>Ph No.   :   +91 - 9556601349</p>
+                    <p>hari @gamil.com</p>
+                    <p><a href="#"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+     <div class="column">
+          <div class="card">
+               <img src="img/subhash.jpg" alt="Subhash" style="width:100%">
+               <div class="container">
+                    <h2>Subhash</h2>
+                    <p class="title">TEAM:- Techie</p>
+                    <p>Ph No.   :   +91 - 8327775034</p>
+                    <p>subhash@gmail.com</p>
+                    <p><a href="#"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+    
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,24 @@
+<?php include('topbar.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title></title>
+</head>
+<body style="background-color: skyblue;">
+    <br><br><br>
+    <div class="box" >
+    	<center><u><h1>Contact Us</h1></u></center>
+         <ul style="font-size: 30px;">
+         	<li>location - Guindy</li>
+         	<li>At - Chennai</li>
+         	<li>po - Guindy</li>
+         	<li>Dist - Chennai</li>
+            <li>Pin - 600015</li>
+         	<li>contact no - +91 6385611298
+         	</li>
+         </ul>
+         <center><h2>Thank you</h2></center>
+    </div>
+</body>
+</html>
